e5ed27d600b76d35851239d34ed0c87c8b9b346a
ErasmoDeveloper <erasmo.network@gmail.com>
2023-02-14 06:06:30 -0300

    Att

---
+++ ProjetoJava02/PrimeiraClasseJava/Aluno.java
@@ -0,0 +1,15 @@
+package PrimeiraClasseJava;
+
+public class Aluno {
+	
+	String nome;
+	int idade;
+	String registroGeral;
+	String numeroCpf;
+	String nomeMae;
+	String nomePai;
+	String dataMatricula;
+	String nomeEscola;
+	String serieMatriculado;
+		
+}
